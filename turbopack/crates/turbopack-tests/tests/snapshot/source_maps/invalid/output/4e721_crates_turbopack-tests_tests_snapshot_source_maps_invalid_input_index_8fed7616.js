7a5f2f489d21ae207bf49eeafcc1168b8a05ce9f
Tim Neutkens <tim@timneutkens.nl>
2025-02-21 14:26:27 +0100

    Update snapshots

--- turbopack/crates/turbopack-tests/tests/snapshot/source_maps/invalid/output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_8fed7616.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/source_maps/invalid/output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_8fed7616.js
@@ -1,7 +1,6 @@
-{
-  "version": 3,
-  "sources": [],
-  "sections": [
-    {"offset": {"line": 6, "column": 0}, "map": {"version":3,"sources":["turbopack:///[project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/invalid/input/index.js"],"sourcesContent":["console.log(\"test\")\n//# sourceMappingURL=index.js.map\n"],"names":[],"mappings":"AAAA,QAAQ,GAAG,CAAC,QACZ,iCAAiC"}},
-    {"offset": {"line": 8, "column": 0}, "map": {"version":3,"sources":[],"names":[],"mappings":"A"}}]
-}
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_8fed7616.js",
+    {},
+    {"otherChunks":["output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_fc734e27.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/invalid/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
