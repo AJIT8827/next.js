48fc7fe5003acaa1fb165296196b76f21b2cbd83
devjiwonchoi <devjiwonchoi@gmail.com>
2025-02-23 12:57:55 +0900

    [dev-overlay] rename `--size-n` css var to `--rem-px-n`

--- packages/next/src/client/components/react-dev-overlay/ui/components/errors/dev-tools-indicator/dev-tools-indicator.tsx
+++ packages/next/src/client/components/react-dev-overlay/ui/components/errors/dev-tools-indicator/dev-tools-indicator.tsx
@@ -252,8 +252,8 @@ function DevToolsPopover({
         // Reset the toast component's default positions.
         bottom: 'initial',
         left: 'initial',
-        [vertical]: 'var(--size-2_5)',
-        [horizontal]: 'var(--size-5)',
+        [vertical]: 'var(--rem-px-10)',
+        [horizontal]: 'var(--rem-px-20)',
       }}
     >
       <NextLogo
@@ -597,13 +597,13 @@ export const DEV_TOOLS_INDICATOR_STYLES = `
 
   .dev-tools-indicator-label {
     font-size: var(--size-font-small);
-    line-height: var(--size-5);
+    line-height: var(--rem-px-20);
     color: var(--color-gray-1000);
   }
 
   .dev-tools-indicator-value {
     font-size: var(--size-font-small);
-    line-height: var(--size-5);
+    line-height: var(--rem-px-20);
     color: var(--color-gray-900);
     margin-left: auto;
   }
@@ -645,11 +645,11 @@ export const DEV_TOOLS_INDICATOR_STYLES = `
 
   .dev-tools-indicator-shortcut {
     display: flex;
-    gap: var(--size-1);
+    gap: var(--rem-px-4);
 
     kbd {
-      width: var(--size-5);
-      height: var(--size-5);
+      width: var(--rem-px-20);
+      height: var(--rem-px-20);
       display: flex;
       justify-content: center;
       align-items: center;
@@ -660,7 +660,7 @@ export const DEV_TOOLS_INDICATOR_STYLES = `
       color: var(--color-gray-1000);
       text-align: center;
       font-size: var(--size-font-smaller);
-      line-height: var(--size-4);
+      line-height: var(--rem-px-16);
     }
   }
 `
